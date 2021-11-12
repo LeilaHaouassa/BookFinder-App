795c2402c7e481ed66089535a808652e7bd7e7c2
Leila Haouassa <laylahawassa@gmail.com>
2021-11-12 16:42:15 +0100

    Integrated the add-new-book feature

--- src/main/java/com/dps/bookfinderapp/controller/BookController.java
+++ src/main/java/com/dps/bookfinderapp/controller/BookController.java
@@ -6,9 +6,12 @@ import com.dps.bookfinderapp.service.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Set;
 
 
@@ -24,7 +27,23 @@ public class BookController {
         Set<Book> books = bookService.findAllByTitleOrAuthor(searchTerm);
         model.addAttribute("books", books);
         return "books/listOfBooks";
+    }
 
+    @GetMapping("/addBook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "books/newBook";
+    }
+
+    @PostMapping("/addBook")
+    public String addBook(@Valid Book book, Model model, BindingResult result) {
+        if (result.hasErrors()) {
+            return "books/newBook";
+        } else {
+            bookService.save(book);
+            model.addAttribute("books", bookService.findAllBooks());
+            return "books/listOfBooks";
+        }
     }
 
 
